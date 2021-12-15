b8685a93665216b32a49099ef5272579dbf2c3ff
Daniel Davis <dannyldnc@gmail.com>
2021-12-15 15:40:59 -0500

    Finish day 8

--- 2021/Day 08/Day08.py
+++ 2021/Day 08/Day08.py
@@ -1,27 +1,61 @@
 def process_line(line):
 	line = line.rstrip().split('|')
-	line = [sorted(line[0].split(), key = len), line[1].split()]
+	line = [sorted([''.join(sorted(x)) for x in line[0].split()], key = len), [''.join(sorted(x)) for x in line[1].split()]]
 	return line
 
 with open('input.txt') as file:
 	lines = file.readlines()
 	lines = [process_line(line) for line in lines]
 
-def str_diff(a, b):
+def str_sub(a, b):
 	for char in b:
 		a = a.replace(char,'')
 	return a
 
 def get_decoder(decode_lst):
+	def get_key(val):
+		for key, value in decode_dict.items():
+			if val == value:
+				return key
+		return None
+	seg = {}
 	decode_dict = {
-		decode_lst[0] : 1,
-		decode_lst[1] : 7,
-		decode_lst[2] : 4,
-		decode_lst[9] : 8
+		decode_lst.pop(0) : 1,
+		decode_lst.pop(0) : 7,
+		decode_lst.pop(0) : 4,
+		decode_lst.pop(6) : 8
 	}
-	seg_a = str_diff(decode_lst[1], decode_lst[0])
-	
-	print(seg_a, decode_dict)
+	seg['a'] = str_sub(get_key(7), get_key(1))
+	for i in range(3, 6):
+		s = str_sub(decode_lst[i], get_key(4) + seg['a'])
+		if len(s) == 1:
+			seg['g'] = s
+			decode_dict[decode_lst.pop(i)] = 9
+			break
+	seg['e'] = str_sub(get_key(8),get_key(9))
+	for i in range(0,3):
+		s = str_sub(decode_lst[i], get_key(1) + seg['a'] + seg['g'])
+		if len(s) == 1:
+			seg['d'] = s
+			decode_dict[decode_lst.pop(i)] = 3
+			break
+	for i in range(0,2):
+		s = str_sub(decode_lst[i], seg['a'] + seg['d'] + seg['e'] + seg['g'])
+		if len(s) == 1:
+			seg['c'] = s
+			decode_dict[decode_lst.pop(i)] = 2
+			break
+	decode_dict[decode_lst.pop(0)] = 5
+	for i in range(0,2):
+		s = str_sub(decode_lst[i], get_key(5))
+		if len(s) == 1:
+			decode_dict[decode_lst.pop(i)] = 6
+			break
+	decode_dict[decode_lst.pop(0)] = 0
+	seg['b'] = str_sub(get_key(5), get_key(3))
+	seg['f'] = str_sub(get_key(3), get_key(2))
+	# print(decode_lst)
+	# print(seg, decode_dict)
 	return decode_dict
 
 def puzzle1():
@@ -36,12 +70,15 @@ def puzzle1():
 	print(f'Puzzle 1 solution: {solution}')
 
 def puzzle2():
-	
+	lst = []
 	for line in lines:
-		# print(line[0])
+		code = ''
 		decoder = get_decoder(line[0])
+		for char in line[1]:
+			code += str(decoder[char])
+		lst.append(int(code))
 	
-	solution = 0
+	solution = sum(lst)
 	
 	print(f'Puzzle 2 solution: {solution}')
 
