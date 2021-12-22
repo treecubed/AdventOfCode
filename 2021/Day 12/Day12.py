fc48a392202da1140c47ad2c07c12921b814e17b
Daniel Davis <dannyldnc@gmail.com>
2021-12-22 17:00:20 -0500

    Continue Day 12

--- 2021/Day 12/Day12.py
+++ 2021/Day 12/Day12.py
@@ -1,5 +1,5 @@
 def process_line(line):
-	line = line.rstrip()
+	line = line.rstrip().split('-')
 	return line
 
 with open('input.txt') as file:
@@ -9,10 +9,20 @@ with open('input.txt') as file:
 # if large letter can go to any child cave
 # small letter can only go to connected large letter or end
 
+def get_valid_links(links, cave, dead_caves):
+	valid_links = []
+	for link in links:
+		if link[0] == cave:
+			if link[1] not in dead_caves:
+				valid_links.append(link)
+		if link[1] == cave:
+			if link[0] not in dead_caves:
+				valid_links.append([link[1],link[0]])
+	return valid_links
 
 def puzzle1():
 	
-	
+	print(get_valid_links(data, 'start', []))
 	
 	solution = 0
 	
