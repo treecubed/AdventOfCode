b9d88ae63658bb02a213a7aac8460dbf3fe63f6d
Daniel Davis <dannyldnc@gmail.com>
2021-12-27 13:15:21 -0500

    Finish Day 12

--- 2021/Day 12/Day12.py
+++ 2021/Day 12/Day12.py
@@ -9,30 +9,70 @@ with open('input.txt') as file:
 # if large letter can go to any child cave
 # small letter can only go to connected large letter or end
 
-def get_valid_links(links, cave, dead_caves):
-	valid_links = []
+def can_revisit(path):
+	for cave in set(path):
+		match cave:
+			case 'start', 'end':
+				continue
+			case _ if cave.islower():
+				if path.count(cave) > 1:
+					return False
+	return True
+
+def cave_valid(path, cave, part_2):
+	if part_2:
+		if can_revisit(path):
+			if cave != 'start' and cave != 'end':
+				return True
+	return cave.lower() not in path
+
+def get_sub_paths(links, path, part_2):
+	sub_paths = []
+	if not path: return []
 	for link in links:
-		if link[0] == cave:
-			if link[1] not in dead_caves:
-				valid_links.append(link)
-		if link[1] == cave:
-			if link[0] not in dead_caves:
-				valid_links.append([link[1],link[0]])
-	return valid_links
+		for i in range(2):
+			if link[i] == path[-1]:
+				if cave_valid(path, link[i-1], part_2):
+					lst = path.copy()
+					lst.append(link[i-1])
+					sub_paths.append(lst)
+	return sub_paths
+
+def get_paths(links, part_2):
+	paths = [['start']]
+	for path in paths:
+		sub_paths = get_sub_paths(links, path, part_2)
+		if sub_paths:
+			paths.extend(sub_paths)
+	
+	pruned_paths = []
+	for path in paths:
+		if path[-1] == 'end':
+			pruned_paths.append(path)
+	
+	return pruned_paths
+
+def display(paths):
+	for path in sorted(paths):
+		print(','.join(path))
 
 def puzzle1():
 	
-	print(get_valid_links(data, 'start', []))
+	paths = get_paths(data, False)
 	
-	solution = 0
+	# display(paths)
+	
+	solution = len(paths)
 	
 	print(f'Puzzle 1 solution: {solution}')
 
 def puzzle2():
 	
+	paths = get_paths(data, True)
 	
+	# display(paths)
 	
-	solution = 0
+	solution = len(paths)
 	
 	print(f'Puzzle 2 solution: {solution}')
 
