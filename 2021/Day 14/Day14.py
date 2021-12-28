e88d75add54dde33b7b7a9e9373d983ffd771180
Daniel Davis <dannyldnc@gmail.com>
2021-12-28 10:39:03 -0500

    Finish Day 14

--- 2021/Day 14/Day14.py
+++ 2021/Day 14/Day14.py
@@ -1,26 +1,66 @@
 from collections import Counter
 
-def process_line(line):
+def process_template(line):
 	line = line.rstrip()
-	return line
+	c = Counter()
+	for i in range(len(line) - 1):
+		c.update([line[i] + line[i + 1]])
+	return c, line
 
-with open('input.txt') as file:
-	data = file.readlines()
-	data = [process_line(line) for line in data]
+def process_rule(line):
+	line = line.rstrip().split()
+	rule = [line[0], [line[0][0] + line[2], line[2] + line[0][1]]]
+	return rule
+
+def read_input():
+	with open('input.txt') as file:
+		data = file.readlines()
+		pairs, template = process_template(data[0])
+		rules = [process_rule(rule) for rule in data[2:]]
+		rules = dict(rules)
+		return pairs, rules, template
+
+def count(pairs, template):
+	cnt = Counter()
+	for i in pairs:
+		cnt[i[0]] += pairs[i]
+	cnt.update(template[-1])
+	return cnt
+
+def do_step(pairs, rules):
+	cnt = Counter()
+	for i in pairs:
+		for rule in rules[i]:
+			cnt[rule] += pairs[i]
+	return cnt
+
+def run_steps(pairs, rules, num):
+	for i in range(num):
+		# print(f'Step {i}')
+		pairs = do_step(pairs, rules)
+	return pairs
 
 def puzzle1():
+	pairs, rules, template = read_input()
 	
+	pairs = run_steps(pairs, rules, 10)
+	cnt = count(pairs, template)
+	mc = cnt.most_common()
+	print(mc)
 	
-	
-	solution = 0
+	solution = mc[0][1] - mc[-1][1]
 	
 	print(f'Puzzle 1 solution: {solution}')
 
 def puzzle2():
+	pairs, rules, template = read_input()
 	
+	pairs = run_steps(pairs, rules, 40)
+	cnt = count(pairs, template)
+	mc = cnt.most_common()
+	print(mc)
 	
-	
-	solution = 0
+	solution = mc[0][1] - mc[-1][1]
 	
 	print(f'Puzzle 2 solution: {solution}')
 
